0894de87d3295ee500568e79d8b2544b04313388
Jeff Soriano <soriano.jeffm@gmail.com>
2024-02-02 14:55:34 -0800

    Split login and signup page into two separate pages

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,27 +1,28 @@
-// pages/LoginPage.tsx
+// pages/SignupPage.tsx
 import React, { useState } from 'react'
 import axios from 'axios'
 import Router from 'next/router'
 import Link from 'next/link'
 import { UserCredentials } from '../app/types'
 
-const LoginPage: React.FC = () => {
-  const [credentials, setCredentials] = useState<
-    Omit<UserCredentials, 'userType'>
-  >({
+const SignupPage: React.FC = () => {
+  const [credentials, setCredentials] = useState<UserCredentials>({
     username: '',
     password: '',
+    userType: 'attendee',
   })
   const [error, setError] = useState<string>('')
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value })
   }
 
-  const handleLogin = async (e: React.FormEvent) => {
+  const handleSignup = async (e: React.FormEvent) => {
     e.preventDefault()
     try {
-      const response = await axios.post('/api/login', credentials)
+      const response = await axios.post('/api/signup', credentials)
       if (response.data.success) {
         Router.push('/dashboard')
       } else {
@@ -36,10 +37,10 @@ const LoginPage: React.FC = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
         <h2 className="text-2xl font-bold mb-8 text-center text-gray-800">
-          Login
+          Sign Up
         </h2>
 
-        <form className="space-y-6" onSubmit={handleLogin}>
+        <form className="space-y-6" onSubmit={handleSignup}>
           <div>
             <label
               htmlFor="username"
@@ -57,7 +58,6 @@ const LoginPage: React.FC = () => {
               onChange={handleChange}
             />
           </div>
-
           <div>
             <label
               htmlFor="password"
@@ -75,26 +75,41 @@ const LoginPage: React.FC = () => {
               onChange={handleChange}
             />
           </div>
+          <div>
+            <label
+              htmlFor="userType"
+              className="block text-sm font-medium text-gray-700"
+            >
+              User Type
+            </label>
+            <select
+              id="userType"
+              name="userType"
+              className="mt-1 block w-full p-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              value={credentials.userType}
+              onChange={handleChange}
+            >
+              <option value="attendee">Convention Attendee</option>
+              <option value="organizer">Convention Organizer</option>
+            </select>
+          </div>{' '}
           <div className="flex items-center justify-between gap-4">
             <button
-              type="submit"
-              className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              onClick={handleLogin}
+              type="button"
+              className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+              onClick={handleSignup}
             >
-              Login
+              Signup
             </button>
-          </div>
+          </div>{' '}
           {error && (
             <p className="mt-4 text-sm text-red-600 text-center">{error}</p>
           )}
         </form>
 
         <div className="mt-4 text-center">
-          <Link
-            className="text-indigo-600 hover:text-indigo-500"
-            href="/signup"
-          >
-            Need an account? Sign up.
+          <Link className="text-indigo-600 hover:text-indigo-500" href="/login">
+            Already have an account? Login.
           </Link>
         </div>
       </div>
@@ -102,4 +117,4 @@ const LoginPage: React.FC = () => {
   )
 }
 
-export default LoginPage
+export default SignupPage
